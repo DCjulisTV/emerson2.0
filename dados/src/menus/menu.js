a35c63a0cdf4517c8dbb0db44585145053a1ff12
Juliano_Schneider <87147350+DCjulisTV@users.noreply.github.com>
2025-07-25 15:49:47 -0300

    Update menu.js

--- dados/src/menus/menu.js
+++ dados/src/menus/menu.js
@@ -4,17 +4,31 @@ async function menu(prefix, botName = "MeuBot", userName = "Usuário") {
 ┊Olá, *${userName}*!
 ╰─┈┈┈┈┈◜❁◞┈┈┈┈┈─╯
 
-╭┈❪🍧ฺꕸ▸ *MENU PRINCIPAL*
+╭┈❪🍧ฺꕸ▸ *MENU*
 ┊
-┊•.̇𖥨֗🍓⭟${prefix}menuia
-┊•.̇𖥨֗🍓⭟${prefix}menudown
 ┊•.̇𖥨֗🍓⭟${prefix}menuadm
 ┊•.̇𖥨֗🍓⭟${prefix}menubn
 ┊•.̇𖥨֗🍓⭟${prefix}menudono
-┊•.̇𖥨֗🍓⭟${prefix}menumemb
-┊•.̇𖥨֗🍓⭟${prefix}ferramentas
-┊•.̇𖥨֗🍓⭟${prefix}menufig
-┊•.̇𖥨֗🍓⭟${prefix}alteradores
+┊•.̇𖥨֗🍓⭟${prefix}gpt
+┊•.̇𖥨֗🍓⭟${prefix}gpt4
+┊•.̇𖥨֗🍓⭟${prefix}tiktok
+┊•.̇𖥨֗🍓⭟${prefix}pinterest
+┊•.̇𖥨֗🍓⭟${prefix}instagram
+┊•.̇𖥨֗🍓⭟${prefix}letra
+┊•.̇𖥨֗🍓⭟${prefix}play
+┊•.̇𖥨֗🍓⭟${prefix}playvid
+┊•.̇𖥨֗🍓⭟${prefix}clima
+┊•.̇𖥨֗🍓⭟${prefix}dicionario
+┊•.̇𖥨֗🍓⭟${prefix}wikipedia
+┊•.̇𖥨֗🍓⭟${prefix}ttp
+┊•.̇𖥨֗🍓⭟${prefix}sticker
+┊•.̇𖥨֗🍓⭟${prefix}ping
+┊•.̇𖥨֗🍓⭟${prefix}perfil
+╭┈❪🍧ฺꕸ▸ *RANKS*
+┊
+┊•.̇𖥨֗🍓⭟${prefix}rankativo
+┊•.̇𖥨֗🍓⭟${prefix}rankinativo
+┊•.̇𖥨֗🍓⭟${prefix}rankativog
 ╰─┈┈┈┈┈◜❁◞┈┈┈┈┈─╯
 `;
 }
